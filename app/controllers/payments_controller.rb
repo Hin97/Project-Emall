24ef247fd7484d5d917258196fd6ec4f41515686
Hin97 <Jacklee5230@hotmail.com>
2019-10-09 05:33:15 +0000

    Paypal respond test 5

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -112,11 +112,11 @@ protected
   def validate_IPN_notification(rawdata)
     sandbox = "#{Rails.application.secrets.sandbox}"
     uri = URI.parse(sandbox + '/webscr?cmd=_notify-validate')
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # http.open_timeout = 60
-    # http.read_timeout = 60
-    # http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    # http.use_ssl = true
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 60
+    http.read_timeout = 60
+    http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+    http.use_ssl = true
     @response = http.post(uri.request_uri, rawdata,
                         'Content-Length' => "#{rawdata.size}",
                         'User-Agent' => "My custom user agent"
